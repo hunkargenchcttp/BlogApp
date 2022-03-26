921194f2bfba9a7bc4e30e6acd862f9bbfeba620
Hünkar Genç Yıldız <hunkar.genc11@hacettepe.edu.tr>
2022-03-26 16:28:05 +0300

    postgresql connection completed

--- blogproject_con/blogproject/settings.py
+++ blogproject_con/blogproject/settings.py
@@ -75,13 +75,23 @@ WSGI_APPLICATION = 'blogproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'BlogApp', 
+        'USER': 'postgres', 
+        'PASSWORD': 'P*b*p575',
+        'HOST': 'localhost', 
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
